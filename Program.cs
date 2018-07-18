090b7731e746d04bfa821838e84f5086fdd46160
ucavalcante <ziel7@hotmail.com>
2018-07-18 16:44:46 -0300

    dotnet new console.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordsInFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
