364e29c33c573cc837954ce3be7ccefb3626ab56
ucavalcante <ziel7@hotmail.com>
2018-07-18 18:33:09 -0300

    Implement Search in console writeLine.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WordsInFiles
 {
@@ -6,7 +7,91 @@ namespace WordsInFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inFile = "";
+            string directoryForRead = "";
+            string outFile = "";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-h":
+                    Console.WriteLine("Help menu");
+                    break;
+                    case "-i":
+                    inFile = args[i+1];
+                    i++;
+                    break;
+                    case "-d":
+                    directoryForRead = args[i+1];
+                    i++;
+                    break;
+                    case "-o":
+                    outFile = args[i+1];
+                    i++;
+                    break;
+                    default:
+                    Console.WriteLine("-h for help");
+                    break;
+                }
+            }
+
+            var usrFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            if (inFile.Contains('~'))
+            {
+                inFile = inFile.Replace("~",usrFolder);
+            }
+            if (directoryForRead.Contains('~'))
+            {
+                directoryForRead = directoryForRead.Replace("~",usrFolder);
+            }
+            if (outFile.Contains('~'))
+            {
+                outFile = outFile.Replace("~", usrFolder);
+            }
+
+            Console.WriteLine($"-i {inFile} -d {directoryForRead} -o {outFile}");
+
+
+
+
+            if (!String.IsNullOrEmpty(inFile))
+            {
+                FileInfo fileInfoIn = new FileInfo(inFile);
+                if (fileInfoIn.Exists)
+                {
+                    using (StreamReader sr = new StreamReader(fileInfoIn.FullName)){
+                        while (sr.Peek() > 0)
+                        {
+                            var line =  sr.ReadLine();
+                            if (!String.IsNullOrEmpty(line) && !String.IsNullOrEmpty(directoryForRead))
+                            {
+                                DirectoryInfo directoryInfoDirectoryForRead = new DirectoryInfo(directoryForRead);
+                                if (directoryInfoDirectoryForRead.Exists)
+                                {
+                                    foreach (var files in directoryInfoDirectoryForRead.GetFiles())
+                                    {
+                                        var lineNumber = 0;
+                                        using (StreamReader fr = new StreamReader(files.FullName)){
+                                            while (sr.Peek() > 0)
+                                            {
+                                                lineNumber++;
+                                                var lineInFile = fr.ReadLine();
+                                                if (!String.IsNullOrEmpty(lineInFile) && lineInFile.Contains(line))
+                                                {
+                                                    Console.WriteLine($"File:{files.Name}|Line Number:{lineNumber}{Environment.NewLine}{lineInFile}");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }    
+            }
+            
         }
     }
 }
